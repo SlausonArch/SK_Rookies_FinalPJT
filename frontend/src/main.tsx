6a801aae7353c2595f05cb759337392008aa97c0
jibin1018 <wlqls1019@gmail.com>
2026-03-25 14:56:00 +0900

    csrf 토큰 제작

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import axios from 'axios'
 import './index.css'
 import App from './App.tsx'
+
+// CSRF: Spring CookieCsrfTokenRepository가 발급하는 XSRF-TOKEN 쿠키를
+// 모든 상태 변경 요청(POST/PUT/PATCH/DELETE)의 X-XSRF-TOKEN 헤더로 자동 첨부
+axios.defaults.withCredentials = true;
+axios.defaults.xsrfCookieName = 'XSRF-TOKEN';
+axios.defaults.xsrfHeaderName = 'X-XSRF-TOKEN';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
